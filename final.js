3704fc9ab41133aa24815c4e06760ec2e97922f7
Skruvkork <wille.nystrom@gmail.com>
2017-02-15 20:07:55 +0100

    wow such almost done

--- final.js
+++ final.js
@@ -1,22 +1,80 @@
 var stuff = Array();
 
-var things = $('such-text');
+var meme = true;
 
-var box = $('saying');
+var things = document.getElementById('such-text');
 
-box.change(see);
+var box = document.getElementById('saying');
 
-function see (doggo) { 
-var words = box does val with ;
+box.addEventListener('change', moar);
+
+// you can always say more! 
+function moar () { 
+var word = box.value;
     
-if (words  === 'doggo'  || words  === 'doge' ) {
-var big = words.toUpperCase();
-stuff.push(big);
-} else {
-stuff.push(words);
+if (word  === 'doggo'  || word  === 'doge' ) {
+word = word.toUpperCase();
 } 
     
-var everything = stuff.join(' ');
-things.html(everything);
-    box.value = ''
+word = makeItem(word);
+meme = true 
+    
+var flyer = document.createElement('div');
+flyer.className = 'flyer' 
+flyer.innerHTML = word.text 
+stuff.push(word);
+things.appendChild(flyer);
+    
+box.value = '' 
 } 
+
+// such animation 
+function magic (thing, there) { 
+var flyers = document.querySelectorAll('.flyer');
+var flyer = flyers[there];
+thing.fly();
+var up = thing.whereUp + 'px' ;
+var left = thing.whereSide + 'px' ;
+var color = thing.color;
+flyer.style.top = up 
+flyer.style.left = left 
+flyer.style.color = colors();
+} 
+
+// not very doge, but it does such job 
+function makeItem (text) { 
+var thingy = {};
+thingy.text = text 
+thingy.whereUp = numbers(window.innerHeight);
+thingy.whereSide = numbers(window.innerWidth);
+thingy.fly = function() { 
+var newUp = numbers(window.innerHeight);
+var newSide = numbers(window.innerWidth);
+        
+this.whereUp = newUp 
+this.whereSide = newSide 
+} 
+thingy.color = colors();
+return thingy;
+} 
+
+// random numbers are hard in doge 
+function numbers (biggest) { 
+var anything = Math.random();
+anything = anything * biggest 
+var low = Math.floor(anything);
+return low;
+} 
+
+function colors () { 
+var red = numbers(255);
+var green = numbers(255);
+var blue = numbers(255);
+var color = `rgb(${red}, ${green}, ${blue})` ;
+return color;
+} 
+
+setInterval(function () {
+var scramble = stuff.map(magic);
+meme = false 
+}, 4000)
---
+++ wow.djs
@@ -0,0 +1,77 @@
+very stuff is plz Array
+
+very meme is true
+
+very things is dogeument dose getElementById with 'such-text'
+
+very box is dogeument dose getElementById with 'saying'
+
+box dose addEventListener with 'change', moar
+
+shh you can always say more!
+such moar
+    very word is box.value
+    
+    rly word is 'doggo' or word is 'doge'
+        word is word dose toUpperCase
+    wow
+    
+    word is plz makeItem with word
+    meme is true
+    
+    very flyer is dogeument dose createElement with 'div'
+    flyer.className is 'flyer'
+    flyer.innerHTML is word.text
+    stuff dose push with word
+    things dose appendChild with flyer
+    
+    box.value is ''
+wow
+
+shh such animation
+such magic much thing there
+    very flyers is dogeument dose querySelectorAll with '.flyer'
+    very flyer is flyers[there]
+    thing dose fly
+    very up is thing.whereUp + 'px'
+    very left is thing.whereSide + 'px'
+    very color is thing.color
+    flyer.style.top is up
+    flyer.style.left is left
+    flyer.style.color is plz colors
+wow 
+
+shh not very doge, but it does such job
+such makeItem much text
+    very thingy is {}
+    thingy.text is text
+    thingy.whereUp is plz numbers with windoge.innerHeight
+    thingy.whereSide is plz numbers with windoge.innerWidth
+    thingy.fly is function() {
+        very newUp is plz numbers with windoge.innerHeight
+        very newSide is plz numbers with windoge.innerWidth
+        
+        this.whereUp is newUp
+        this.whereSide is newSide
+    wow
+    thingy.color is plz colors
+wow thingy
+
+shh random numbers are hard in doge
+such numbers much biggest
+    very anything is Math dose random
+    anything is anything * biggest
+    very low is Math dose floor with anything
+wow low
+
+such colors
+    very red is plz numbers with 255
+    very green is plz numbers with 255
+    very blue is plz numbers with 255
+    very color is `rgb(${red}, ${green}, ${blue})`
+wow color
+
+plz setInterval with much
+    very scramble is stuff dose map with magic
+    meme is false
+}, 4000)
